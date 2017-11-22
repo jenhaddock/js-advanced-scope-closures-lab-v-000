b1ec4f20b1eddb06513f3b7b2d188ce4de300606
Jennifer Haddock <jentx123@hotmail.com>
2017-11-22 04:30:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+produceDrivingRange(firstBlock, secondBlock){
+  return function(range){
+    let blockDiff = secondBlock - firstBlock;
+    if (blockDiff < range){
+      return `within range by ${range - blockDiff}`
+    } else {
+      return `${blockDiff - range} blocks out of range`
+    }
+    
+  }  
+}
